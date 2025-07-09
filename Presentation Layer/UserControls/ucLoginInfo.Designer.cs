b434cc937de4a70924e4555468f126a549f5779a
I3mk132 <ma066084@gmail.com>
2025-07-09 12:07:19 +0300

    User details design done

---
+++ Presentation Layer/UserControls/ucLoginInfo.Designer.cs
@@ -0,0 +1,191 @@
+﻿namespace Presentation_Layer.UserControls
+{
+    partial class ucLoginInfo
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.cbLoginInfo = new System.Windows.Forms.GroupBox();
+            this.lblIsActiveValue = new System.Windows.Forms.Label();
+            this.lblUserID = new System.Windows.Forms.Label();
+            this.lblUserIDTitle = new System.Windows.Forms.Label();
+            this.lblIsActiveTitle = new System.Windows.Forms.Label();
+            this.lblUsernameTitle = new System.Windows.Forms.Label();
+            this.pbIsActive = new System.Windows.Forms.PictureBox();
+            this.pbUserNo = new System.Windows.Forms.PictureBox();
+            this.pbUsername = new System.Windows.Forms.PictureBox();
+            this.lblUsernameValue = new System.Windows.Forms.Label();
+            this.cbLoginInfo.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pbIsActive)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbUserNo)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbUsername)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // cbLoginInfo
+            // 
+            this.cbLoginInfo.Controls.Add(this.lblUsernameValue);
+            this.cbLoginInfo.Controls.Add(this.lblIsActiveValue);
+            this.cbLoginInfo.Controls.Add(this.lblUserID);
+            this.cbLoginInfo.Controls.Add(this.lblUserIDTitle);
+            this.cbLoginInfo.Controls.Add(this.pbIsActive);
+            this.cbLoginInfo.Controls.Add(this.pbUserNo);
+            this.cbLoginInfo.Controls.Add(this.pbUsername);
+            this.cbLoginInfo.Controls.Add(this.lblIsActiveTitle);
+            this.cbLoginInfo.Controls.Add(this.lblUsernameTitle);
+            this.cbLoginInfo.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cbLoginInfo.Location = new System.Drawing.Point(13, 10);
+            this.cbLoginInfo.Name = "cbLoginInfo";
+            this.cbLoginInfo.Size = new System.Drawing.Size(738, 91);
+            this.cbLoginInfo.TabIndex = 87;
+            this.cbLoginInfo.TabStop = false;
+            this.cbLoginInfo.Text = "Login info";
+            // 
+            // lblIsActiveValue
+            // 
+            this.lblIsActiveValue.AutoSize = true;
+            this.lblIsActiveValue.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblIsActiveValue.ForeColor = System.Drawing.Color.Black;
+            this.lblIsActiveValue.Location = new System.Drawing.Point(618, 33);
+            this.lblIsActiveValue.Name = "lblIsActiveValue";
+            this.lblIsActiveValue.Size = new System.Drawing.Size(40, 24);
+            this.lblIsActiveValue.TabIndex = 92;
+            this.lblIsActiveValue.Text = "???";
+            // 
+            // lblUserID
+            // 
+            this.lblUserID.AutoSize = true;
+            this.lblUserID.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblUserID.ForeColor = System.Drawing.Color.Black;
+            this.lblUserID.Location = new System.Drawing.Point(145, 33);
+            this.lblUserID.Name = "lblUserID";
+            this.lblUserID.Size = new System.Drawing.Size(40, 24);
+            this.lblUserID.TabIndex = 86;
+            this.lblUserID.Text = "???";
+            // 
+            // lblUserIDTitle
+            // 
+            this.lblUserIDTitle.AutoSize = true;
+            this.lblUserIDTitle.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblUserIDTitle.ForeColor = System.Drawing.Color.Black;
+            this.lblUserIDTitle.Location = new System.Drawing.Point(19, 33);
+            this.lblUserIDTitle.Name = "lblUserIDTitle";
+            this.lblUserIDTitle.Size = new System.Drawing.Size(76, 20);
+            this.lblUserIDTitle.TabIndex = 85;
+            this.lblUserIDTitle.Text = "User ID:";
+            // 
+            // lblIsActiveTitle
+            // 
+            this.lblIsActiveTitle.AutoSize = true;
+            this.lblIsActiveTitle.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblIsActiveTitle.ForeColor = System.Drawing.Color.Black;
+            this.lblIsActiveTitle.Location = new System.Drawing.Point(480, 32);
+            this.lblIsActiveTitle.Name = "lblIsActiveTitle";
+            this.lblIsActiveTitle.Size = new System.Drawing.Size(88, 20);
+            this.lblIsActiveTitle.TabIndex = 53;
+            this.lblIsActiveTitle.Text = "Is Active: ";
+            // 
+            // lblUsernameTitle
+            // 
+            this.lblUsernameTitle.AutoSize = true;
+            this.lblUsernameTitle.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblUsernameTitle.ForeColor = System.Drawing.Color.Black;
+            this.lblUsernameTitle.Location = new System.Drawing.Point(248, 33);
+            this.lblUsernameTitle.Name = "lblUsernameTitle";
+            this.lblUsernameTitle.Size = new System.Drawing.Size(65, 20);
+            this.lblUsernameTitle.TabIndex = 47;
+            this.lblUsernameTitle.Text = "Name: ";
+            // 
+            // pbIsActive
+            // 
+            this.pbIsActive.Image = global::Presentation_Layer.Properties.Resources.Key;
+            this.pbIsActive.Location = new System.Drawing.Point(574, 31);
+            this.pbIsActive.Name = "pbIsActive";
+            this.pbIsActive.Size = new System.Drawing.Size(25, 25);
+            this.pbIsActive.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pbIsActive.TabIndex = 80;
+            this.pbIsActive.TabStop = false;
+            // 
+            // pbUserNo
+            // 
+            this.pbUserNo.Image = global::Presentation_Layer.Properties.Resources.Tag;
+            this.pbUserNo.Location = new System.Drawing.Point(101, 32);
+            this.pbUserNo.Name = "pbUserNo";
+            this.pbUserNo.Size = new System.Drawing.Size(25, 25);
+            this.pbUserNo.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pbUserNo.TabIndex = 74;
+            this.pbUserNo.TabStop = false;
+            // 
+            // pbUsername
+            // 
+            this.pbUsername.Image = global::Presentation_Layer.Properties.Resources.Person;
+            this.pbUsername.Location = new System.Drawing.Point(319, 31);
+            this.pbUsername.Name = "pbUsername";
+            this.pbUsername.Size = new System.Drawing.Size(25, 25);
+            this.pbUsername.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pbUsername.TabIndex = 73;
+            this.pbUsername.TabStop = false;
+            // 
+            // lblUsernameValue
+            // 
+            this.lblUsernameValue.AutoSize = true;
+            this.lblUsernameValue.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblUsernameValue.ForeColor = System.Drawing.Color.Black;
+            this.lblUsernameValue.Location = new System.Drawing.Point(361, 33);
+            this.lblUsernameValue.Name = "lblUsernameValue";
+            this.lblUsernameValue.Size = new System.Drawing.Size(40, 24);
+            this.lblUsernameValue.TabIndex = 93;
+            this.lblUsernameValue.Text = "???";
+            // 
+            // ucLoginInfo
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.cbLoginInfo);
+            this.Name = "ucLoginInfo";
+            this.Size = new System.Drawing.Size(765, 113);
+            this.cbLoginInfo.ResumeLayout(false);
+            this.cbLoginInfo.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pbIsActive)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbUserNo)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbUsername)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox cbLoginInfo;
+        private System.Windows.Forms.Label lblIsActiveValue;
+        private System.Windows.Forms.Label lblUserID;
+        private System.Windows.Forms.Label lblUserIDTitle;
+        private System.Windows.Forms.PictureBox pbIsActive;
+        private System.Windows.Forms.PictureBox pbUserNo;
+        private System.Windows.Forms.PictureBox pbUsername;
+        private System.Windows.Forms.Label lblIsActiveTitle;
+        private System.Windows.Forms.Label lblUsernameTitle;
+        private System.Windows.Forms.Label lblUsernameValue;
+    }
+}
---
+++ Presentation Layer/UserControls/ucUsersList.Designer.cs
@@ -0,0 +1,195 @@
+﻿namespace Presentation_Layer.UserControls
+{
+    partial class ucUsersList
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.lblRecordCount = new System.Windows.Forms.Label();
+            this.tableLayoutPanel2 = new System.Windows.Forms.TableLayoutPanel();
+            this.cbFilterBy = new System.Windows.Forms.ComboBox();
+            this.lblFilter = new System.Windows.Forms.Label();
+            this.txtFilter = new System.Windows.Forms.TextBox();
+            this.dgvPersonsList = new System.Windows.Forms.DataGridView();
+            this.chkIsActive = new System.Windows.Forms.CheckBox();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.tableLayoutPanel2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvPersonsList)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 1;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.Controls.Add(this.lblRecordCount, 0, 2);
+            this.tableLayoutPanel1.Controls.Add(this.tableLayoutPanel2, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.dgvPersonsList, 0, 1);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 3;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 40F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 33F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(811, 340);
+            this.tableLayoutPanel1.TabIndex = 11;
+            // 
+            // lblRecordCount
+            // 
+            this.lblRecordCount.AutoSize = true;
+            this.lblRecordCount.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblRecordCount.ForeColor = System.Drawing.Color.DimGray;
+            this.lblRecordCount.Location = new System.Drawing.Point(3, 307);
+            this.lblRecordCount.Name = "lblRecordCount";
+            this.lblRecordCount.Size = new System.Drawing.Size(155, 26);
+            this.lblRecordCount.TabIndex = 16;
+            this.lblRecordCount.Text = "# Records: -1";
+            // 
+            // tableLayoutPanel2
+            // 
+            this.tableLayoutPanel2.ColumnCount = 4;
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 15.52795F));
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 17.76398F));
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 29.31677F));
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 37.3913F));
+            this.tableLayoutPanel2.Controls.Add(this.cbFilterBy, 1, 0);
+            this.tableLayoutPanel2.Controls.Add(this.lblFilter, 0, 0);
+            this.tableLayoutPanel2.Controls.Add(this.txtFilter, 2, 0);
+            this.tableLayoutPanel2.Controls.Add(this.chkIsActive, 3, 0);
+            this.tableLayoutPanel2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel2.Location = new System.Drawing.Point(3, 3);
+            this.tableLayoutPanel2.Name = "tableLayoutPanel2";
+            this.tableLayoutPanel2.RowCount = 1;
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel2.Size = new System.Drawing.Size(805, 34);
+            this.tableLayoutPanel2.TabIndex = 0;
+            this.tableLayoutPanel2.Paint += new System.Windows.Forms.PaintEventHandler(this.tableLayoutPanel2_Paint);
+            // 
+            // cbFilterBy
+            // 
+            this.cbFilterBy.BackColor = System.Drawing.SystemColors.Control;
+            this.cbFilterBy.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.cbFilterBy.DropDownHeight = 220;
+            this.cbFilterBy.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cbFilterBy.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cbFilterBy.FormattingEnabled = true;
+            this.cbFilterBy.IntegralHeight = false;
+            this.cbFilterBy.Items.AddRange(new object[] {
+            "None",
+            "Person ID",
+            "National No.",
+            "First Name",
+            "Second Name",
+            "Third Name",
+            "Last Name",
+            "Nationality",
+            "Gender",
+            "Phone",
+            "Email"});
+            this.cbFilterBy.Location = new System.Drawing.Point(128, 3);
+            this.cbFilterBy.MaxDropDownItems = 11;
+            this.cbFilterBy.Name = "cbFilterBy";
+            this.cbFilterBy.Size = new System.Drawing.Size(135, 28);
+            this.cbFilterBy.TabIndex = 12;
+            // 
+            // lblFilter
+            // 
+            this.lblFilter.AutoSize = true;
+            this.lblFilter.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblFilter.ForeColor = System.Drawing.Color.DimGray;
+            this.lblFilter.Location = new System.Drawing.Point(3, 0);
+            this.lblFilter.Name = "lblFilter";
+            this.lblFilter.Size = new System.Drawing.Size(115, 26);
+            this.lblFilter.TabIndex = 11;
+            this.lblFilter.Text = "Filter By: ";
+            // 
+            // txtFilter
+            // 
+            this.txtFilter.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtFilter.Location = new System.Drawing.Point(271, 3);
+            this.txtFilter.Name = "txtFilter";
+            this.txtFilter.Size = new System.Drawing.Size(227, 26);
+            this.txtFilter.TabIndex = 13;
+            this.txtFilter.Visible = false;
+            // 
+            // dgvPersonsList
+            // 
+            this.dgvPersonsList.AllowUserToAddRows = false;
+            this.dgvPersonsList.AllowUserToDeleteRows = false;
+            this.dgvPersonsList.BackgroundColor = System.Drawing.SystemColors.Control;
+            this.dgvPersonsList.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.dgvPersonsList.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvPersonsList.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dgvPersonsList.Location = new System.Drawing.Point(3, 43);
+            this.dgvPersonsList.MultiSelect = false;
+            this.dgvPersonsList.Name = "dgvPersonsList";
+            this.dgvPersonsList.ReadOnly = true;
+            this.dgvPersonsList.Size = new System.Drawing.Size(805, 261);
+            this.dgvPersonsList.TabIndex = 15;
+            // 
+            // chkIsActive
+            // 
+            this.chkIsActive.Anchor = System.Windows.Forms.AnchorStyles.Left;
+            this.chkIsActive.AutoSize = true;
+            this.chkIsActive.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.chkIsActive.Location = new System.Drawing.Point(507, 5);
+            this.chkIsActive.Name = "chkIsActive";
+            this.chkIsActive.Size = new System.Drawing.Size(84, 24);
+            this.chkIsActive.TabIndex = 14;
+            this.chkIsActive.Text = "IsActive";
+            this.chkIsActive.UseVisualStyleBackColor = true;
+            this.chkIsActive.Visible = false;
+            // 
+            // ucUsersList
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Transparent;
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Name = "ucUsersList";
+            this.Size = new System.Drawing.Size(811, 340);
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel1.PerformLayout();
+            this.tableLayoutPanel2.ResumeLayout(false);
+            this.tableLayoutPanel2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvPersonsList)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.Label lblRecordCount;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel2;
+        private System.Windows.Forms.ComboBox cbFilterBy;
+        private System.Windows.Forms.Label lblFilter;
+        private System.Windows.Forms.TextBox txtFilter;
+        private System.Windows.Forms.DataGridView dgvPersonsList;
+        private System.Windows.Forms.CheckBox chkIsActive;
+    }
+}
