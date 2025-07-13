a916b0556906eefe6e5c9bcfb85de1d11c5ecad7
I3mk132 <ma066084@gmail.com>
2025-07-13 12:15:54 +0300

    clsLocalDrivingLicenseApplicationsDataAccess
    Add method added

--- Data Layer/LocalDrivingLicenseApplicationsDataAccess.cs
+++ Data Layer/LocalDrivingLicenseApplicationsDataAccess.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,6 +9,43 @@ namespace Data_Layer
 {
     public class clsLocalDrivingLicenseApplicationsDataAccess
     {
+        public static int Add(int ApplicationID, int LicenseClassID)
+        {
+            SqlConnection connection = new SqlConnection(clsSettings.ConnectionString);
 
+            string query = @"
+
+                INSERT INTO LocalDrivingLicenseApplications 
+                (ApplicationID, LicenseClassID) Values (@ApplicationID, @LicenseClassID);
+                SELECT SCOPE_IDENTITY();
+
+            ";
+
+            SqlCommand command = new SqlCommand(query, connection);
+
+            command.Parameters.AddWithValue("@ApplicationID", ApplicationID);
+            command.Parameters.AddWithValue("@LicenseClassID", LicenseClassID);
+
+            int ID = -1;
+            try
+            {
+                connection.Open();
+                object result = command.ExecuteScalar();
+
+                if (result != null && int.TryParse(result.ToString(), out int id))
+                    ID = id;
+
+            }
+            catch (Exception ex)
+            {
+                clsErrorLog.AddErrorLog(ex);
+            }
+            finally
+            {
+                connection.Close();
+            }
+            return ID;
+
+        }
     }
 }
