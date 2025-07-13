e1bcc3a6728a6a459d1ae57496f8824eed97e071
I3mk132 <ma066084@gmail.com>
2025-07-13 11:24:44 +0300

    GetAllLicenseClassNames i is Added

--- Data Layer/LicenseClassesDataAccess.cs
+++ Data Layer/LicenseClassesDataAccess.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,6 +9,36 @@ namespace Data_Layer
 {
     public class clsLicenseClassesDataAccess
     {
+        public static List<string> GetAllLicenseClassNames()
+        {
+            SqlConnection connection = new SqlConnection(clsSettings.ConnectionString);
+
+            string query = @"SELECT ClassName FROM LicenseClasses";
+
+            SqlCommand command = new SqlCommand(query, connection);
+
+            List<string> LicenseClasses = new List<string>();
+            try
+            {
+                connection.Open();
+
+                SqlDataReader reader = command.ExecuteReader();
+                while (reader.Read())
+                {
+                    LicenseClasses.Add(reader.GetString(0));
+                }
+                reader.Close();
+            }
+            catch (Exception ex)
+            {
+                clsErrorLog.AddErrorLog(ex);
+            }
+            finally
+            {
+                connection.Close();
+            }
+            return LicenseClasses;
+        }
 
     }
 }
