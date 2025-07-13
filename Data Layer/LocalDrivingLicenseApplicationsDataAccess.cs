4aabe8b8b8c72b87bfe8db7ec2a8c4c5b7070197
I3mk132 <ma066084@gmail.com>
2025-07-13 16:03:37 +0300

    GetAll() in clsLocalDrivingLicenseApplicationsDataAccess is DONE

--- Data Layer/LocalDrivingLicenseApplicationsDataAccess.cs
+++ Data Layer/LocalDrivingLicenseApplicationsDataAccess.cs
@@ -1,7 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Data.SqlClient;
 using System.Linq;
+using System.Net;
+using System.Security.Policy;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -47,5 +50,64 @@ namespace Data_Layer
             return ID;
 
         }
+        public static DataTable GetAll()
+        {
+            SqlConnection connection = new SqlConnection(clsSettings.ConnectionString);
+
+            string query = @"
+
+                SELECT 
+	                LD.LocalDrivingLicenseApplicationID AS [L.D.L.APPID],
+	                LC.ClassName AS [Driving Class],
+	                P.NationalNo AS [National No.],
+	                P.Firstname + ' ' + P.Secondname + ' ' + P.Thirdname + ' ' + P.Lastname AS [Fullname],
+	                A.ApplicationDate AS [Application Date],
+	                (
+	                Select count(*)
+	                From Tests T
+		                JOIN TestAppointments TA ON T.TestAppointmentID = TA.TestAppointmentID
+		                JOIN LocalDrivingLicenseApplications LD2 ON TA.LocalDrivingLicenseApplicationID = LD2.ApplicationID
+		                JOIN Applications A2 ON A2.ApplicationID = LD2.ApplicationID
+	                WHERE 
+		                A2.PersonID = P.PersonID AND T.TestResult = 1
+	                ) AS [Passed Tests],
+	                CASE A.ApplicationStatus
+		                WHEN 1 THEN 'New' 
+		                WHEN 2 THEN 'Cancelled' 
+		                ELSE 'Other'
+	                END AS Status
+
+                FROM LocalDrivingLicenseApplications LD
+	                JOIN Applications A ON A.ApplicationID = LD.ApplicationID
+	                JOIN LicenseClasses LC ON LD.LicenseClassID = LC.LicenseClassID
+	                JOIN Person P ON A.PersonID = P.PersonID;";
+
+            SqlCommand command = new SqlCommand(query, connection);
+
+            DataTable dt = new DataTable();
+            try
+            {
+                connection.Open();
+                SqlDataReader reader = command.ExecuteReader();
+
+                if (reader.HasRows)
+                {
+                    dt.Load(reader);
+                }
+                reader.Close();
+            }
+            catch (Exception ex)
+            {
+                clsErrorLog.AddErrorLog(ex);
+            }
+            finally
+            {
+                connection.Close();
+            }
+            return dt;
+
+        }
+
+
     }
 }
