a1324f4a7e1d789df6043cc911f90da50ee796e5
I3mk132 <ma066084@gmail.com>
2025-07-23 19:16:14 +0300

    usercontrol created

---
+++ Presentation Layer/UserControls/ApplicationsUserControl/ucDLApplicationInfo.Designer.cs
@@ -0,0 +1,45 @@
+﻿namespace Presentation_Layer.UserControls.ApplicationsUserControl
+{
+    partial class ucDLApplicationInfo
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // ucDLApplicationInfo
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Name = "ucDLApplicationInfo";
+            this.Size = new System.Drawing.Size(680, 221);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
